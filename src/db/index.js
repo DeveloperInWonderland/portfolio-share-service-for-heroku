1a8a9ad53d65fee12397da28afe94051fa1911fe
Hyejin Eom <hjeom.dev@gmail.com>
2022-04-02 16:02:26 +0900

    Style: mongodb url 없는 경우 처리하는 문자열 삭제

--- src/db/index.js
+++ src/db/index.js
@@ -6,7 +6,7 @@ import { Certificate } from "./models/Certificate";
 import { Project } from "./models/Project";
 import { Blog } from "./models/Blog";
 
-const DB_URL = process.env.MONGODB_URL || "MongoDB 서버 주소가 설정되지 않았습니다.\n./db/index.ts 파일을 확인해 주세요.";
+const DB_URL = process.env.MONGODB_URL;
 
 mongoose.connect(DB_URL);
 const db = mongoose.connection;
